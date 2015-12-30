5e81f2aba4124f36dadff4132d1d57568abefe0d
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-12-30 14:20:17 -0600

    Code from "Rendering a list of things."
    Remove multiple roots. Change component to render a list of animals.

--- src/reagent_tutorial/core.cljs
+++ src/reagent_tutorial/core.cljs
@@ -7,20 +7,28 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello Reagent World!"}))
+(defonce app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
 
 (defn hello-world []
-  [:p (:text @app-state)])
+  ;; The following code works; however, it generates a warning in the developer console:
+  ;;     Every element in a seq should have a unique :key
+  ;; This warning results from React expecting each element in the virtual DOM to have a unique key so that it can more
+  ;; effectively determine changes to elements. My guess is that the Om library takes care of this issue for me.
+  ;;
+  ;; For more information, see the post on StackOverflow,
+  ;; http://stackoverflow.com/questions/33446913/reagent-react-clojurescript-warning-every-element-in-a-seq-should-have-a-unique  
+  ;; 
+  #_[:ul (map #(vector :li %) (:list @app-state))]
+  ;; 
+  ;; To resolve this warning, the aforementioned post suggested adding meta data to each element in the list. The metadata is
+  ;; a map between the symbol :key and a unique item (the list item if it is unique; other suggestions were using `gen-key`
+  ;; or using a UUID generator).
+  ;; 
+  [:ul (map #(with-meta (vector :li %) {:key %}) (:list @app-state))]
+  )
 
 (reagent/render-component [hello-world]
-                          (. js/document (getElementById "app0")))
-
-(defn alt-hello-world []
-  [:h1 (:text @app-state)])
-
-(reagent/render-component [alt-hello-world]
-                          (. js/document (getElementById "app1")))
-
+                          (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
